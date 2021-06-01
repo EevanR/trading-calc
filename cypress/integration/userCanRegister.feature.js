03deb23408152b7615a7f821f875668d92206c15
Eevan <eevan.redon@gmail.com>
2021-06-01 14:53:38 +0200

    adds restration info and  register.json to stubb /auth call

--- cypress/integration/userCanRegister.feature.js
+++ cypress/integration/userCanRegister.feature.js
@@ -2,10 +2,26 @@ describe("User can register", () => {
   beforeEach(() => {
     cy.viewport(1350, 900);
     cy.server();
-    cy.visit("/");
   })
 
-  it("can successfully register a new account", () => {
+  it("can successfully access Register Tab", () => {
+    cy.visit("/");
     cy.contains("Register").click();
+  })
+
+  it("can submit registration", () => {
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/auth",
+      response: "fixture:register.json",
+      status: 200
+    })
+    cy.get("#signup-form").within(() => {
+      cy.get("#email").type("newtrader@mail.com");
+      cy.get("#username").type("NewTrader");
+      cy.get("#password").type("password");
+      cy.get("#passCon").type("password");
+      cy.get("#submit").click();
+    })
   })
 })
