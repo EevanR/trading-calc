5e002315ca473143db44475d87f42f0772f3a1ba
Eevan <eevan.redon@gmail.com>
2021-07-22 17:31:42 +0200

    adds twitter.jsx component file, and twitter feature cypress

---
+++ cypress/integration/userCanSeeTwitterUpdates.feature.js
@@ -0,0 +1,23 @@
+describe("User can see live tweets", () => {
+  beforeEach(() => {
+    cy.viewport(1450,1000);
+    cy.server()
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/trades",
+      response: "fixture:saved_trades.json",
+      status: 200
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/setups",
+      response: "fixture:index_strategies.json",
+      status: 200,
+    })
+  })
+
+  it("can successfully see tweets from favorite user", () => {
+    cy.login()
+
+  })
+})
