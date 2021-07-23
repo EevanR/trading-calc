e6912723562cc06fdb6cb736f89b5a2efd53854b
Eevan <eevan.redon@gmail.com>
2021-07-23 22:35:43 +0200

    adds input field for twitter users

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 
 const Twitter = () => {
 
+  const submit = (e) => {
+    e.preventdefault();
+  }
+
 
   return (
     <>
       <h1>Twitter Feed</h1>
+      <h4>Live updates from User:</h4>
+      <form id="twitter-form" onSubmit={submit}>
+        <div>
+          <label>Username</label>
+          <input 
+            required
+            type="text"
+            placeholder="Twitter Handle"
+            name="username"
+            id="username"
+          />
+        </div>
+      </form>
     </>
   )
 }
