953b611a53144782f498954228bc1f4c8a9b4ad3
Eevan <eevan.redon@gmail.com>
2021-09-13 22:34:34 +0200

    removing all previous css, rebuilding

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -7,6 +7,7 @@ import { connect } from "react-redux";
 const Signin = props => {
   const [redirect, setRedirect] = useState(false)
   const [loader, setLoader] = useState(false)
+  const image1 = './screenshots/wallstr.png'
 
   const submitFormHandler = async event => {
     event.preventDefault();
@@ -106,17 +107,80 @@ const Signin = props => {
   ]
 
   return (
-    <div>
-      {redirect === true && <Redirect to='/panes'/>}
-      <div className="signin-box">
-        <Tab panes={panes} />
-        {loader === true && (
-          <Dimmer active>
-            <Loader />
-          </Dimmer>
-        )}
-      </div>
-    </div>
+    <>
+      <header className="bg-primary text-center">
+        <div className="container container--narrow">
+          <h1>Welcome to Tradelogs, Do lots of things with your logs</h1>
+          <p>Below find information and sign up blah blah blah</p>
+        </div>
+      </header>
+
+      <section className="bg-light">
+        <div className="container">
+          <h2 className="text-center">Section 1</h2>
+          <div className="split">
+            <div>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Why do we use it?
+              </p>
+            </div>
+            <div>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Why do we use it?
+              </p>
+            </div>
+            <div>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                Why do we use it?
+              </p>
+            </div>
+            {/* {redirect === true && <Redirect to='/panes'/>}
+            <div className="signin-box">
+              <Tab panes={panes} />
+              {loader === true && (
+                <Dimmer active>
+                  <Loader />
+                </Dimmer>
+              )}
+            </div> */}
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-dark">
+        <div className="container">
+          <div className="split">
+            <div>
+              <h2>First Section</h2>
+              <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Why do we use it?
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+              </p>
+            </div>
+            <div>
+              <img src={image1} alt="Wall Street" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-primary">
+        <div className="container">
+          <div className="split">
+            <div>
+              <h2>Another section</h2>
+              <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+            </div>
+            <div>
+          
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
