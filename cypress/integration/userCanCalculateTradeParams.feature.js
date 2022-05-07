c682022cb0bd9f971c02f50afde755507bfac2cf
Eevan <eevan.redon@gmail.com>
2022-05-07 09:20:36 +0200

    adds userCanCalculateTradeParams.feature.js in cypress

---
+++ cypress/integration/userCanCalculateTradeParams.feature.js
@@ -0,0 +1,35 @@
+describe("User can calculate trade params on calculator tab", () => {
+  beforeEach(() => {
+    cy.viewport(1450, 1000);
+    cy.server()
+    cy.requests();
+    cy.login();
+  })
+
+  it("can successfully delete a setup", () => {
+    cy.contains("Strategies").click();
+    cy.route({
+      method: "DELETE",
+      url: "http://localhost:3000/api/v1/setups/**",
+      response: "fixture:delete_setup_success.json",
+      headers: {
+        uid: "trader@mail.com"
+      },
+      status: 200
+    })
+
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/setups",
+      response: "fixture:reload_strat.json",
+      status: 200,
+    })
+
+    // cy.get("#setup1").within(() => {
+    //   cy.get("#1").click({force: true})
+    // })
+
+    // cy.get("#result-message").should("contain", '"Strat 1" Deleted')
+    // cy.get("#setup1").should('not.be.visible')
+  })
+})
