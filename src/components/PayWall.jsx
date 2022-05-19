bf68ec177edb45f68a20dd9e3f6e037ef8380dcc
Eevan <eevan.redon@gmail.com>
2022-05-19 05:53:55 +0200

    adds stripe publish key nd logic to PayWall.jsx

--- src/components/PayWall.jsx
+++ src/components/PayWall.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
 const PayWall = () => {
+  const [message, setMessage] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [sessionId, setSessionId] = useState('');
   
+  const stripe = loadStripe("pk_test_51Ky6crB3yNNgOUzgR2446d6HO2cSm8pSo8QNMAh11pFYnyTBNKE3sQ0fZEjLM3NpGTL5G2kOqJrbgmoKgI3LilHq008UE6519r")
 
-
+  const SuccessDisplay = ({ sessionId }) => {
+    return (
+      <section>
+        <div className="product Box-root">
+          <div className="description Box-root">
+            <h3>Subscription to starter plan successful!</h3>
+          </div>
+        </div>
+        <form action="/create-portal-session" method="POST">
+          <input
+            type="hidden"
+            id="session-id"
+            name="session_id"
+            value={sessionId}
+          />
+          <button id="checkout-and-portal-button" type="submit">
+            Manage your billing information
+          </button>
+        </form>
+      </section>
+    );
+  };
+  
+  const Message = ({ message }) => (
+    <section>
+      <p>{message}</p>
+    </section>
+  );
+  
   return (
     <section className="paywall">
       <div>
