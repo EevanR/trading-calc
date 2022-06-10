cca45084c3ff343e683addb9512cddf070d77c4b
Eevan <eevan.redon@gmail.com>
2022-06-10 05:50:29 +0200

    adds Canceled.jsx and Success.jsx

---
+++ src/components/Canceled.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Canceled = () => {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Canceled;
--- src/components/Panes.jsx
+++ src/components/Panes.jsx
@@ -74,7 +74,7 @@ const Panes = () => {
   }, []);
 
   if (!success && message === '') {
-    return null
+    
   } else if (success && sessionId !== '') {
     alert(message)
   } else {
