7c31a2b32b51a803ea7654037af79b3eb6add431
Eevan <eevan.redon@gmail.com>
2022-06-12 09:42:06 +0200

    adds user info to payment success page, adds styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Panes from './components/Panes'
 import Signin from './components/Signin'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "@stripe/stripe-js"
+import Canceled from './components/Canceled';
+import Success from './components/Success';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Signin} />
           <Route exact path="/panes" component={Panes} />
+          <Route exact path="/success" component={Success} />
+          <Route exact path="/canceled" component={Canceled} />
         </Switch>
       </BrowserRouter>
       </div>
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Success = () => {
+  const [user, setUser] = useState({})
+  
+  let date = Date().slice(0, 15)
+  useEffect(() => {
+    let storage = JSON.parse(sessionStorage.getItem('user'))
+    setUser(storage)
+  }, [])
+  
 
   return (
     <>
-    
+      <div className="panes bg-dark">
+        <div className="two-column-grid">
+          <div>
+            <a href="/"><img src="/TradeLogs.png"  alt="TradeLogs Logo"/></a>
+          </div>
+          <div>
+  
+          </div>
+        </div>
+      </div>
+      <div className="success-page">
+        <h1><i class="check circle icon"></i>SUCCESS</h1>
+        <h4>Your TradeLogs subscription has successfully registered.</h4>
+        <button className="oval-button-white">Manage Subscription</button>
+        <div className="box">
+          <div className="two-column-grid">
+            <div>
+              <p className="left-align">Customer Email</p>
+              <p className="left-align">Date</p>
+              <p className="left-align">Transaction Status</p>
+              <p className="left-align">Subscription Period</p>
+              <p className="left-align">Payment Amount</p>
+            </div>
+            <div>
+              <p className="right-align">{user.email}</p>
+              <p className="right-align">{date}</p>
+              <p className="right-align green">Success</p>
+              <p className="right-align">Monthly</p>
+              <p className="right-align">$12.00</p>
+            </div>
+          </div>
+        </div>
+        <p> <a>&larr; back to TradeLogs dashboard</a></p>
+      </div>
     </>
   )
 }
