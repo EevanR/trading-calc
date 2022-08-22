5222a0447f33455092c2c9385758df41b25ae170
Eevan <eevan.redon@gmail.com>
2022-08-22 06:00:12 +0200

    adds upload trades and upload fees instructions to HowTo.jsx

--- src/components/HowTo.jsx
+++ src/components/HowTo.jsx
@@ -15,16 +15,31 @@ const HowTo = () => {
         <p>Register account and log in</p>
 
         <h3>Dashboard</h3>
-        <p>Welcome to TradeLogs! Once logged in, familiarize yourself with the dashboard and the various secion tabs.</p>
-        <p>Clicking the TradeLogs Logo at the top left of the page will take you back to the Sign in page, logging you out.</p>
+        <p>Welcome to TradeLogs! Once logged in, familiarize yourself with the dashboard and the various section tabs.</p>
+        <p>Clicking the TradeLogs Logo at the top left of the page will take you back to the Home page, logging you out.</p>
 
         <h3>Upload Trades</h3>
         <p>Currently Supported Brokers: TradeZero </p>
         <p>
           1. Log in to your TradeZero account
           <br />
-          2. Head over to Account Summary
+          2. Head over to Account Overview
+          <br />
+          3. Scroll down and select the heading "Trades".
+          <br />
+          4. Select the desired date range.
+          <br />
+          5. Click the Excel symbol to download the file.
+          <br />
+          6. Open the file, and select the file menu dropdown.
+          <br />
+          7. Select "Save as" and re-save as "Excel Workbook (.xlsx)".
+          <br />
+          8. Return to www.trade-logs.com, Sign in, and upload the newly saved .xlsx file.
         </p>
+
+        <h3>Upload Software and Locate Fees</h3>
+        <p>Repeat steps for Upload Trades. Except go to your "Cash Journal" tab instead of your "Trades" tab.</p>
       </section>
       <section className="section-6 bg-dark">
         <div className="container-wide">
