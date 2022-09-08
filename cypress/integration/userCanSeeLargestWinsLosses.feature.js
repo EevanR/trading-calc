974d984fc673d5b0c27183433066640efa05c435
Eevan <eevan.redon@gmail.com>
2022-09-08 05:29:57 +0200

    adds userCanSeeLargestWinLoss.feature.js

---
+++ cypress/integration/userCanSeeLargestWinsLosses.feature.js
@@ -0,0 +1,12 @@
+describe("User can see largest wind and largest loss", () => {
+  beforeEach(() => {
+    cy.viewport(1450,1000);
+    cy.server()
+    cy.login()
+    cy.requests()
+  })
+
+  it("can successfully set desired user twitter handle", () => {
+    
+  })
+})
