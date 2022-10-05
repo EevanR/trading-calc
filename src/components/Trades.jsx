2f125bf7d0685d357e8c65d49d7100f419a7a3f4
Eevan <eevan.redon@gmail.com>
2022-10-05 20:34:30 +0200

    adds footer and foreground div to Trades.jsx

--- src/components/Trades.jsx
+++ src/components/Trades.jsx
@@ -5,7 +5,20 @@ const Trades = () => {
 
   return (
     <div>
-      <h1>stuff</h1>
+      <section className="bg-verydark" id="graphs">
+        <div className="container-wide">
+          <div className="foreground bg-dark equity">
+            <h3 className="left-align">Preformance</h3>
+            <h4 className="left-align">Ticker List</h4>
+            <div className="border-top">
+
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="footer bg-dark">
+        <p >Copyright © TradeLogs 2022. All rights reserved.</p>
+      </section>
     </div>
   )
 };
