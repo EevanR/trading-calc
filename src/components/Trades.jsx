2777945b14560668b6a387d5a820eb62dfbd29fe
Eevan <eevan.redon@gmail.com>
2022-10-24 06:01:39 +0200

    adds redux to trades history tab

--- src/components/Trades.jsx
+++ src/components/Trades.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from "react-redux";
 
-const Trades = () => {
+const Trades = props => {
   
+  let tradeHistory;
+  if (props.savedTrades !== null) {
+    tradeHistory = props.savedTrades.map(entry => {
+      return (
+        <>
+        
+        </>
+      )
+    })
+  } else {
+    tradeHistory = "No Trade History"
+  }
 
   return (
     <div>
-      <section className="bg-verydark" id="graphs">
+      <section className="bg-verydark trades" id="graphs">
         <div className="container-wide">
           <div className="foreground bg-dark equity">
             <h3 className="left-align">Preformance</h3>
@@ -23,4 +36,10 @@ const Trades = () => {
   )
 };
 
-export default Trades;
+const mapStateToProps = state => {
+  return {
+    savedTrades: state.savedTrades
+  };
+};
+
+export default connect(mapStateToProps)(Trades)
