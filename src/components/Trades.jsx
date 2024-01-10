7f1f2734ff2d26b8104b3cb59fc1d4607892cccd
Eevan <eevan.redon@gmail.com>
2024-01-10 20:05:57 +0100

    refactors Trades.jsx, adds .toUpperCase method to ticker when searched through savedTrades

--- src/components/Trades.jsx
+++ src/components/Trades.jsx
@@ -2,52 +2,54 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Form } from 'semantic-ui-react'
 
-const Trades = props => {
-  const [tradesArray, setTradesArray] = useState([])
+const Trades = ({ savedTrades }) => {
+  const [tradesArray, setTradesArray] = useState([]);
 
   const searchHistory = (e) => {
     e.preventDefault();
-    let ticker = e.target.ticker.value
-    searchTrades(ticker)
-  }
+    let ticker = e.target.ticker.value;
+    searchTrades(ticker);
+  };
 
-  let inputs;
-  const searchTrades =  (ticker) => {
-    let newArray = []
-    for (let i=0; i<props.savedTrades.data.length; i++) {
-      if (props.savedTrades.data[i]['Ticker'] === ticker) {
-        newArray.push(props.savedTrades.data[i])
+  const searchTrades = (ticker) => {
+    debugger
+    let newArray = [];
+    for (let i = 0; i < savedTrades.data.length; i++) {
+      if (savedTrades.data[i]['Ticker'] === ticker.toUpperCase()) {
+        newArray.push(savedTrades.data[i]);
       }
     }
-    debugger
-  }
+    debugger;
+  };
 
   useEffect(() => {
-
-  }, [])
+    // Add any logic you need to run after the component mounts
+  }, []);
 
   return (
     <>
       <section className="bg-verydark trades" id="graphs">
         <div className="container-wide">
           <div className="foreground bg-dark">
-          <Form onSubmit={searchHistory}>
-            <input placeholder="Ticker" required type='text' name="ticker" id="ticker"/>
-            <button className="ui button" id="loadChart" type="submit">Search</button>
-          </Form>       
+            <Form onSubmit={searchHistory}>
+              <input placeholder="Ticker" required type="text" name="ticker" id="ticker" />
+              <button className="ui button" id="loadChart" type="submit">
+                Search
+              </button>
+            </Form>
           </div>
         </div>
       </section>
       <section className="footer bg-dark">
-        <p >Copyright © TradeLogs 2022. All rights reserved.</p>
+        <p>Copyright © TradeLogs 2022. All rights reserved.</p>
       </section>
     </>
-  )
+  );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    savedTrades: state.savedTrades
+    savedTrades: state.savedTrades,
   };
 };
 
