3a535ab44bf31f43e72226010f50370d43c0d0f2
Eevan <eevan.redon@gmail.com>
2024-01-11 17:59:42 +0100

    adds findStats() method to organize ticker stats

--- src/components/Trades.jsx
+++ src/components/Trades.jsx
@@ -4,27 +4,37 @@ import { Form } from 'semantic-ui-react'
 
 const Trades = ({ savedTrades }) => {
   const [tradesArray, setTradesArray] = useState([]);
+  const [stats, setStats] = useState({ Wins: 0, PnL: 0, Largest: 0 });
 
   const searchHistory = (e) => {
     e.preventDefault();
-    let ticker = e.target.ticker.value;
+    const ticker = e.target.ticker.value;
     searchTrades(ticker);
   };
 
   const searchTrades = (ticker) => {
-    let newArray = [];
-    for (let i = 0; i < savedTrades.data.length; i++) {
-      if (savedTrades.data[i]['Ticker'] === ticker.toUpperCase()) {
-        newArray.push(savedTrades.data[i]);
-      }
-    }
-    debugger
-    setTradesArray(newArray)
+    const newArray = savedTrades.data.filter((trade) => trade.Ticker === ticker.toUpperCase());
+    setTradesArray(newArray);
+    findStats(newArray);
   };
 
-  useEffect(() => {
-    // Add any logic you need to run after the component mounts
-  }, []);
+  const findStats = (trades) => {
+    const newStats = trades.reduce(
+      (accumulator, trade) => {
+        const grossProfit = Number(trade.GrossProfit.toFixed(2));
+        if (grossProfit < 0) {
+          accumulator.Wins++;
+          accumulator.PnL += grossProfit;
+          accumulator.Largest = Math.max(accumulator.Largest, grossProfit);
+        } else {
+          accumulator.Smallest = Math.min(accumulator.Smallest, grossProfit);
+        }
+        return accumulator;
+      },
+      { Wins: 0, PnL: 0, Largest: 0, Smallest: 0 }
+    );
+    setStats(newStats);
+  };
 
   return (
     <>
@@ -42,10 +52,15 @@ const Trades = ({ savedTrades }) => {
         {tradesArray.length > 0 && (
           <div>
             <h2>New Array Contents:</h2>
+            <h3># of Trades: {tradesArray.length}</h3>
             <p>
-              {tradesArray.map((item, index) => (
-                <li key={index}>{item.Ticker}</li>
-              ))}
+              Track Record: {stats.Wins} Wins out of {tradesArray.length} trades
+              <br />
+              Total Profits: ${stats.PnL}
+              <br />
+              Largest Winning Trade: ${stats.Largest}
+              <br />
+              Largst Losing Trade: ${stats.Smallest}
             </p>
           </div>
         )}
