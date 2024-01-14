2031552692018ca5cf2b9c19be7cf5dd26913b61
Eevan <eevan.redon@gmail.com>
2024-01-14 20:52:10 +0100

    rewrite userCanLogin.cy.js to updated cypress

--- cypress/e2e/userCanLogin.cy.js
+++ cypress/e2e/userCanLogin.cy.js
@@ -1,32 +1,26 @@
 describe("User can log in", () => {
-  it('should visi', () => {
-    cy.visit('/')
-  })
-  // beforeEach(() => {
-  //   cy.viewport(1350, 900);
-  //   cy.server();
-  //   cy.requests()
-  //   cy.route({
-  //     method: "POST",
-  //     url: "http://localhost:3000/api/v1/auth/sign_in",
-  //     response: "fixture:login.json",
-  //     headers: {
-  //       uid: "trader@mail.com"
-  //     },
-  //     status: 200,
-  //     delay: 1000
-  //   });
-  //   cy.visit("/");
-
-  it("successfully loads login page", () => {
-    cy.contains("Quick. Easy. Tradelogs.")
-  })
-
-  it("can successfully sign in", () => {
-    cy.contains("Sign In").click()
-  cy.get("#email").type("trader@mail.com");
-  cy.get("#password").type("password");
-  cy.get("#submit").click();
-    cy.contains("Profit Curve")
+  beforeEach(() => {
+    cy.intercept('POST', 'http://localhost:3000/api/v1/auth/sign_in', {
+      fixture: 'login.json',
+      headers: {
+        uid: 'trader@mail.com'
+      },
+      statusCode: 200,
+      delayMs: 1000
+    });
   });
+  
+  it('successfully loads', () => {
+    cy.visit('/') 
+
+    cy.contains("Quick. Easy. Tradelogs.")
+
+    cy.contains("Sign In").click()
+    cy.get("#email").type("trader@mail.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.contains("Profit Curve")
+  })
+
 });
