0f4a5de46232fe62bc95588734281bddbbb685a7
Eevan <eevan.redon@gmail.com>
2024-01-14 20:22:15 +0100

    adds cypress test file, nav to localhost

--- cypress/e2e/userCanLogin.cy.js
+++ cypress/e2e/userCanLogin.cy.js
@@ -1,20 +1,22 @@
 describe("User can log in", () => {
-  beforeEach(() => {
-    cy.viewport(1350, 900);
-    cy.server();
-    cy.requests()
-    cy.route({
-      method: "POST",
-      url: "http://localhost:3000/api/v1/auth/sign_in",
-      response: "fixture:login.json",
-      headers: {
-        uid: "trader@mail.com"
-      },
-      status: 200,
-      delay: 1000
-    });
-    cy.visit("/");
-  });
+  it('should visi', () => {
+    cy.visit('/')
+  })
+  // beforeEach(() => {
+  //   cy.viewport(1350, 900);
+  //   cy.server();
+  //   cy.requests()
+  //   cy.route({
+  //     method: "POST",
+  //     url: "http://localhost:3000/api/v1/auth/sign_in",
+  //     response: "fixture:login.json",
+  //     headers: {
+  //       uid: "trader@mail.com"
+  //     },
+  //     status: 200,
+  //     delay: 1000
+  //   });
+  //   cy.visit("/");
 
   it("successfully loads login page", () => {
     cy.contains("Quick. Easy. Tradelogs.")
