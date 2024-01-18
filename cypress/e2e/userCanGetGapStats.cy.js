1343841171e947ec6465f5b2e22e0eb37f4f9c41
Eevan <eevan.redon@gmail.com>
2024-01-18 14:18:27 +0100

    updates structure of userCanGetGapStats.cy.js

--- cypress/e2e/userCanGetGapStats.cy.js
+++ cypress/e2e/userCanGetGapStats.cy.js
@@ -1,31 +1,27 @@
 describe("User can get gap stats", () => {
   beforeEach(() => {
     cy.viewport(1350, 1200)
-    cy.server();
     cy.login();
     cy.requests();
   });
 
   it("successfully submit ticker", () => {
     cy.contains("Gap Stats").click();
-    cy.route({
-      method: "GET",
-      url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY**",
-      response: "fixture:intraday_fiveMinute.json",
-      status: 200
+    cy.intercept('GET', 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY**', {
+      fixture: 'intraday_fiveMinute.json',
+      statusCode: 200
     });
-    cy.route({
-      method: "GET",
-      url: "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY**",
-      response: "fixture:daily_request.json",
-      status: 200
+
+    cy.intercept('GET', 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY**', {
+      fixture: 'daily_request.json',
+      statusCode: 200
     });
-    cy.route({
-      method: "GET",
-      url: "https://www.alphavantage.co/query?function=VWAP**",
-      response: "fixture:vwap.json",
-      status: 200
+
+    cy.intercept('GET', 'https://www.alphavantage.co/query?function=VWAP**', {
+      fixture: 'vwap.json',
+      statusCode: 200
     });
+
     cy.get("#testTicker").type("IBM");
     cy.get("#loadChart").click();
 
