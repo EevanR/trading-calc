b30891f204d4dbf19e9c082b2ef1fd0d82ab175b
Eevan <eevan.redon@gmail.com>
2024-01-20 09:36:45 +0100

    updates structure for userCanEditSetup.cy.js

--- cypress/e2e/userCanEditSetup.cy.js
+++ cypress/e2e/userCanEditSetup.cy.js
@@ -1,33 +1,29 @@
 describe("User can edit a setup", () => {
-  // beforeEach(() => {
-  //   cy.viewport(1450, 1000);
-  //   cy.server();
-  //   cy.requests();
-  // })
+  beforeEach(() => {
+    cy.requests()
+  });
 
-  // it("can successfully edit setup", () => {
-  //   cy.login(); 
-  //   cy.route({
-  //     method: "PATCH",
-  //     url: "http://localhost:3000/api/v1/setups/**",
-  //     response: "fixture:setup_update.json",
-  //     status: 200
-  //   });
+  it("can successfully edit setup", () => {
+    cy.login(); 
+    cy.intercept('PATCH', 'http://localhost:3000/api/v1/setups/**', {
+      fixture: 'setup_update.json',
+      statusCode: 200
+    });
 
-  //   cy.contains("Strategies").click();
-  //   cy.get("#setup1").within(() => {
-  //     cy.get("#edit1").click()
-  //   })
+    cy.contains("Strategies").click();
+    cy.get("#setup1").within(() => {
+      cy.get("#edit1").click()
+    });
 
-  //   cy.route({
-  //     method: "GET",
-  //     url: "http://localhost:3000/api/v1/setups",
-  //     response: "fixture:updated_index_strats.json",
-  //     status: 200,
-  //   })
+    cy.intercept('GET', 'http://localhost:3000/api/v1/setups', {
+      fixture: 'updated_index_strats.json',
+      statusCode: 200
+    });
 
-  //   cy.get('#req3').type("Add a new requirement");
-  //   cy.contains("Save Edit").click()
-  //   cy.get("#setup1").should("contain", "Add a new requirement");
-  // })
+    cy.get('#req3').type("Add a new requirement");
+    cy.contains("Save Edit").click();
+
+    cy.get('#result-message').should('contain', 'Strat 1 updated Successfully')
+    cy.get("#setup1").should("contain", "Add a new requirement");
+  })
 }) 
