a329a5fbc83cf42b0af823ece96cec30a1b1eadb
Eevan <eevan.redon@gmail.com>
2024-01-20 13:58:42 +0100

    updates to userCanSaveStrategy.cy.js

--- cypress/e2e/useCanSaveStrategy.cy.js
+++ cypress/e2e/useCanSaveStrategy.cy.js
@@ -1,41 +1,37 @@
 describe("User can save strategy", () => {
-  // beforeEach(() => {
-  //   cy.viewport(1450, 1000);
-  //   cy.server();
-  //   cy.requests();
-  // })
+  beforeEach(() => {
+    cy.login();
+    cy.requests();
+  });
 
-  // it("can successfully save strategy", ()=> {    
-  //   cy.login();
-  //   cy.route({
-  //     method: "POST",
-  //     url: "http://localhost:3000/api/v1/setups",
-  //     response: "fixture:save_setup_success.json",
-  //     status: 200
-  //   })
+  it("can successfully save strategy", ()=> {   
+    cy.intercept('POST', 'http://localhost:3000/api/v1/setups', {
+      fixture: 'save_setup_success.json',
+      statusCode: 200
+    });
 
-  //   cy.contains("Strategies").click();
-  //   cy.get(".strats-form").within(() => {
-  //     cy.get("#name").type("Strategy 1");
-  //     cy.get("#req1").type("Stock in play");
-  //     cy.get("#req2").type("Strong relative volume");
-  //   })
-  //   cy.contains("Create Strategy").click();
-  //   cy.get("#result-message").should("contain", "Strategy 1 added Successfully")
-  // })
+    cy.contains("Strategies").click();
+    cy.get(".strats-form").within(() => {
+      cy.get("#name").type("Strategy 1");
+      cy.get("#req1").type("Stock in play");
+      cy.get("#req2").type("Strong relative volume");
+    })
+    cy.contains("Create Strategy").click();
+    cy.get("#result-message").should("contain", "Strategy 1 added Successfully")
+  });
 
-  // it("can unsuccessfully save strategy", () => {
-  //   cy.route({
-  //     method: "POST",
-  //     url: "http://localhost:3000/api/v1/setups",
-  //     response: "fixture:failed_saved_strategy.json",
-  //     status: 422
-  //   })
-  //   cy.get(".strats-form").within(() => {
-  //     cy.get("#name").clear();
-  //     cy.contains("Create Strategy").click();
-  //   })
+  it("can unsuccessfully save strategy", () => {
+    cy.intercept('POST', 'http://localhost:3000/api/v1/setups', {
+      fixture: 'failed_saved_strategy.json',
+      statusCode: 422
+    });
 
-  //   cy.get("#result-message").should("contain", "Name can't be blank")
-  // })
+    cy.contains("Strategies").click();
+    cy.get(".strats-form").within(() => {
+      cy.get("#name").clear();
+      cy.contains("Create Strategy").click();
+    });
+
+    cy.get("#result-message").should("contain", "Name can't be blank");
+  })
 })
