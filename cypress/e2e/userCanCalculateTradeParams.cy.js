441f8f31901159f135ed3c241b849183db45771c
Eevan <eevan.redon@gmail.com>
2024-01-20 10:54:42 +0100

    updates structure for userCanCalculateTradeParams.cy.js

--- cypress/e2e/userCanCalculateTradeParams.cy.js
+++ cypress/e2e/userCanCalculateTradeParams.cy.js
@@ -17,11 +17,23 @@ describe("User can calculate trade params on calculator tab", () => {
 
   it("can successfully see saved strategies", () => {
     cy.requests();
+    cy.wait(5000);
 
-    cy.contains("Calculator").click()
+    cy.contains("Calculator").click();
+
+    cy.get(".setups-inner").should("contain", "Strat 1");
+
+    cy.get('#equity').type(10000);
+    cy.get('#ticker').type('DRYS');
+    cy.get('#price').type(2.00);
+    cy.get('#stop').type(2.20);
+
     cy.get("#setup-dropdown").click()
     cy.get('.selected > .text').click()
+    cy.get('.two-column-grid > :nth-child(1) > label').click()
+    cy.get('.two-column-grid > :nth-child(2) > label').click()
+    cy.get('#calculate').click()
 
-    cy.get(".setups-inner").should("contain", "Strat 1")
+    cy.get('section > .two-column-grid > :nth-child(2) > :nth-child(2)').should('contain', -1000);
   })
 })
